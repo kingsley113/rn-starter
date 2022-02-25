87e6d7c4bfd13670eebe65d29bd77e2ab0c036cc
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-24 17:53:21 -0800

    Create button components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello, Cameron!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hello, Cameron!</Text>
+      <Button
+        onPress={() => console.log("Button pressed")}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity
+        onPress={() => console.log("Touchable Opacity pressed")}
+      >
+        <Text>Go to Lists Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({

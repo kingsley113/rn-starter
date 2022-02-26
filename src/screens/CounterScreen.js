6af2806794fde0bda2fa998e190fc8552c4ca0fb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-25 20:56:21 -0800

    Convert counter screen to reducer hook

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase_count":
+      return { ...state, counter: state.counter + 1 };
+    case "decrease_count":
+      return { ...state, counter: state.counter - 1 };
+    case "reset_count":
+      return { ...state, counter: 0 };
+    default:
+      return;
+  }
+};
 
+const CounterScreen = () => {
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
   return (
     <View>
       <Text>Current Count: {counter}</Text>
       <Button
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increase_count" });
         }}
         title="Increase Count"
       />
       <Button
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrease_count" });
         }}
         title="Decrease Count"
       />
       <Button
         onPress={() => {
-          setCounter(0);
+          dispatch({ type: "reset_count" });
         }}
         title="Reset"
       />

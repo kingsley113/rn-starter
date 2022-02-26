9fb2c01e8cf7bf8645911944bc1179a4ea3e75c2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-02-25 22:13:45 -0800

    Add password field with validation

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Button, Text, TextInput } from "react-native";
 
 const TextScreen = () => {
   const [name, setName] = useState(" ");
+  const [password, setPassword] = useState(" ");
 
   return (
     <View>
@@ -16,6 +17,20 @@ const TextScreen = () => {
         value={name}
       />
       <Text>Hello, my name is: {name}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(newValue) => {
+          setPassword(newValue);
+        }}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+      />
+      {password.length <= 5 ? (
+        <Text style={styles.password}>
+          Password must be 5 or more characters.
+        </Text>
+      ) : null}
     </View>
   );
 };
@@ -26,6 +41,9 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     fontSize: 30,
+  },
+  password: {
+    color: "red",
   },
 });
 
